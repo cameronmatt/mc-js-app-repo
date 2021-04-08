87402acef8eeef0818ef4984a1d1f9ce92c92fb8
Matthew <cameron.matt@outlook.com>
2021-04-09 09:47:28 +1000

    aremoved duplication of listing container

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 const apikey = 'key_ce7f356ae4d82efac11bfba518b392f2'
 let listings = [];
 let savedSearch = [];
+let filteredListings = [];
 // fetch files from domain.com.au API
 function fetchListings() {
     const response = fetch(`https://api.domain.com.au/v1/listings/residential/_search?api_key=${apikey}&HTTP/1.1/`, {
@@ -23,19 +24,7 @@ function fetchListings() {
           //console.log(data);
           let html = '';
           data.forEach( async function(address){
-            html += `
-                <li> 
-                  <img id="propetyImage" src="${address.project.media[0].url}"></img>
-                  <h3>${address.project.displayableAddress}</h3>
-                  <span id = "heart"><i class="fa fa-heart-o" aria-hidden="true" style="font-size:24px"></i> </span>
-                  <h2>${address.listings[0].priceDetails.displayPrice}</h2>
-                  <p>
-                    <i class="fa fa-bed"></i> <a>${address.listings[0].propertyDetails.bedrooms}</a> 
-                    <i class="fa fa-car"></i> <a>${address.listings[0].propertyDetails.carspaces?address.listings[0].propertyDetails.carspaces:0}</a> 
-                    <i class="fa fa-bath"></i> <a>${address.listings[0].propertyDetails.bathrooms}</a>
-                    </p>
-                </li>
-            `;
+            html += listingDisplay(address);
             listings.push(address);
           });
           document.getElementById("listings").innerHTML = html;
@@ -52,51 +41,71 @@ function fetchListings() {
   searchBar.addEventListener('keydown', (e) => {
         if (e.key === 'Enter'){
         const searchString = e.target.value.toUpperCase();
-        const filteredListings = listings.filter( address => {
+        //searchBar.value("");
+        let filteredListings = listings.filter( address => {
               return (
                 address.project.suburb.includes(searchString)
-              );
-        });
+              );            
+        });          
         //console.log(filteredListings);
         let html = '';
         filteredListings.forEach(function (address){ 
         // Display main image, address, price, Bedrooms, Bathrooms, Carspaces, Like button
-          html += `
-              <li> 
-                  <img id="propetyImage" src="${address.project.media[0].url}"></img>
-                  <h3>${address.project.displayableAddress}</h3>
-                  <span id = "heart"><i class="fa fa-heart-o" aria-hidden="true" style="font-size:24px"></i> </span>
-                  <h2>${address.listings[0].priceDetails.displayPrice}</h2>
-                  <p>
-                    <i class="fa fa-bed"></i> <a>${address.listings[0].propertyDetails.bedrooms}</a> 
-                    <i class="fa fa-car"></i> <a>${address.listings[0].propertyDetails.carspaces?address.listings[0].propertyDetails.carspaces:0}</a> 
-                    <i class="fa fa-bath"></i> <a>${address.listings[0].propertyDetails.bathrooms}</a>
-                  </p>
-              </li>
-          `;
+          html += listingDisplay(address);
+          console.log(address)
           //Take the submitted search and enter as a saved search on sidebar
           savedSearch.push(searchString);
           document.getElementById("searches").innerHTML = "Recent Search";
-              for (var i = 0; i < savedSearch.length; i++)
               {
-                  nameList = "<ul id=listy >" + savedSearch[i] + "</ul>";
+                  nameList = "<li class=listy >" + savedSearch + "</li>";
                   document.getElementById("searches").innerHTML += nameList;
               }
         });
         document.getElementById("listings").innerHTML = html;
       }
-      document.addEventListener('click',function () {
-      let clickSaved = document.getElementById("listy");
-      clickSaved.onclick = function() {
-        alert(savedSearch);
-      }; 
-    }); 
+      
+      let clickSaved = document.getElementsByClassName("listy");
+      console.log(clickSaved)
+    //   clickSaved.onclick = function() {
+    //     console.log(savedSearch);
+    //     let filteredListings = listings.filter( address => {
+    //       return (
+    //         address.project.suburb.includes(savedSearch[0])
+    //       );                   
+    // //console.log(filteredListings);
+    // let html = '';
+    // filteredListings.forEach(function (address){ 
+    // // Display main image, address, price, Bedrooms, Bathrooms, Carspaces, Like button
+    //   html += listingDisplay(address);
+    //   //Take the submitted search and enter as a saved search on sidebar
+    // });
+    // document.getElementById("listings").innerHTML = html;
+
+    //   }; 
+    // }); 
   });
+
+function listingDisplay(address) {
+  return `
+  <li> 
+    <img id="propetyImage" src="${address.project.media[0].url}"></img>
+    <h3>${address.project.displayableAddress}</h3>
+    <span id = "heart"><i class="fa fa-heart-o" aria-hidden="true" style="font-size:24px"></i> </span>
+    <h2>${address.listings[0].priceDetails.displayPrice}</h2>
+    <p>
+      <i class="fa fa-bed"></i> <a>${address.listings[0].propertyDetails.bedrooms}</a> 
+      <i class="fa fa-car"></i> <a>${address.listings[0].propertyDetails.carspaces?address.listings[0].propertyDetails.carspaces:0}</a> 
+      <i class="fa fa-bath"></i> <a>${address.listings[0].propertyDetails.bathrooms}</a>
+    </p>
+  </li>
+`;
+}
+
 
 
   document.addEventListener('click', likeHeart)
-  function likeHeart(heart) {
-    heart = heart.target
+  function likeHeart(event) {
+    heart = event.target
     if ( heart.classList.contains( "fa-heart") ) {
         heart.classList.remove( "fa-heart" );
         heart.classList.add( "fa-heart-o" );
--- index1.js
+++ index1.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetchListings();
-});
 
+//link to blue house image: http://eagle-host.s3-us-west-2.amazonaws.com/uploads/dd1fd1a85dcc8811cdb6d4a51826497b/blue-house.png
 
 const apikey = 'key_ce7f356ae4d82efac11bfba518b392f2'
 let listings = [];
-
+let savedSearch = [];
+let filteredListings = [];
+let html = '';
 // fetch files from domain.com.au API
 function fetchListings() {
-    const response = fetch(`https://api.domain.com.au/v1/listings/residential/_search?api_key=${apikey}&HTTP/1.1/?_limit=2`, {
+    const response = fetch(`https://api.domain.com.au/v1/listings/residential/_search?api_key=${apikey}&HTTP/1.1/`, {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -22,14 +22,8 @@ function fetchListings() {
       })
       .then(function(data){
           //console.log(data);
-          let html = '';
-          data.forEach(function(address){
-            html += `
-                <li>
-                    <img id="propetyImage" src="${address.project.media[0].url}"></img>
-                    <h2>${address.project.displayableAddress}</h2>
-                </li>
-            `;
+          data.forEach( async function(address){
+            html += listingDisplay(address);
             listings.push(address);
           });
           document.getElementById("listings").innerHTML = html;
@@ -40,72 +34,110 @@ function fetchListings() {
       })
   }
   fetchListings();
-//Building the search function. not sure if Response in variable above is correct. 
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Search bar that can search for suburbs
   const searchBar = document.getElementById("search-bar");
-  //console.log(searchBar);
-  searchBar.addEventListener('keyup', (e) => {
-      const searchString = e.target.value;
-      const filteredListings = listings.filter( address => {
-            return (
-              address.project.suburb.includes(searchString)
-            );
-      });
-      console.log(filteredListings);
-      let html = '';
-      filteredListings.forEach(function(address){
-        html += `
-            <li>
-                <img id="propetyImage" src="${address.project.media[0].url}"></img>
-                <h2>${address.project.displayableAddress}</h2>
-            </li>
-        `;
-      });
-      document.getElementById("listings").innerHTML = html;
+//console.log(searchBar);
+  searchBar.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter'){
+        const searchString = e.target.value.toUpperCase();
+        //searchBar.value("");
+        let filteredListings = listings.filter( address => {
+              return (
+                address.project.suburb.includes(searchString)
+              );            
+        });          
+        //console.log(filteredListings);
+        filteredListings.forEach(function (address){ 
+        // Display main image, address, price, Bedrooms, Bathrooms, Carspaces, Like button
+          html += listingDisplay(address);
+          console.log(address)
+          //Take the submitted search and enter as a saved search on sidebar
+          savedSearch.push(searchString);
+          document.getElementById("searches").innerHTML = "Recent Search";
+              {
+                  nameList = "<li class=listy >" + savedSearch + "</li>";
+                  document.getElementById("searches").innerHTML += nameList;
+              }
+              {
+                  searchIn = "<h3 id=homes-in >" + savedSearch + "</h3>";
+                  document.getElementById("homes-for-sale").innerHTML += searchIn;
+              }
+            
+        });
+        document.getElementById("listings").innerHTML = html;
+      }
+      
+  //     let clickSaved = document.getElementsByClassName("listy");
+  //     console.log(clickSaved)
+  //        clickSaved.onclick = function() {
+  //   //     console.log(savedSearch);
+  //   //     let filteredListings = listings.filter( address => {
+  //   //       return (
+  //   //         address.project.suburb.includes(savedSearch[0])
+  //   //       );                   
+  //   // //console.log(filteredListings);
+  //   // let html = '';
+  //   // filteredListings.forEach(function (address){ 
+
+  //   //   }; 
+  //   // }); 
   });
 
+        let clickSaved = document.getElementsByClassName("listy");
+      console.log(clickSaved)
+         clickSaved.onclick = function() {
+        console.log(savedSearch);
+        let filteredListings = listings.filter( address => {
+          return (
+            address.project.suburb.includes(savedSearch[0])
+          );                   
+    //console.log(filteredListings);
+    }); 
+  }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//This is the container for listings data
+// Displays main image, address, price, Bedrooms, Bathrooms, Carspaces, Like button
+function listingDisplay(address) {
+  return `
+  <ul> 
+    <img id="propetyImage" src="${address.project.media[0].url}"></img>
+    <h3>${address.project.displayableAddress}</h3>
+    <span id = "heart"><i class="fa fa-heart-o" aria-hidden="true" style="font-size:24px"></i> </span>
+    <h2>${address.listings[0].priceDetails.displayPrice}</h2>
+    <a>
+      <i class="fa fa-bed"></i> <a>${address.listings[0].propertyDetails.bedrooms}</a> 
+      <i class="fa fa-car"></i> <a>${address.listings[0].propertyDetails.carspaces?address.listings[0].propertyDetails.carspaces:0}</a> 
+      <i class="fa fa-bath"></i> <a>${address.listings[0].propertyDetails.bathrooms}</a>
+    </a>
+  </ul>
+`;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//Liker heart. Would like to add a counter to this. 
+  document.addEventListener('click', likeHeart)
+  function likeHeart(event) {
+    heart = event.target
+    if ( heart.classList.contains( "fa-heart") ) {
+        heart.classList.remove( "fa-heart" );
+        heart.classList.add( "fa-heart-o" );
+    }
+    else if ( heart.classList.contains( "fa-heart-o") ) {
+        heart.classList.remove( "fa-heart-o" );
+        heart.classList.add( "fa-heart" );
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
 // Get all suburbs from listings on Domain
 // Store suburbs for purpose of predictive text in search bar
-// Search bar that can search for suburbs
-//
+
 //
 // Display in listings section, date ordered newest to oldest
-// Display Bedrooms, Bathrooms, Carspaces
-// Display price
-// Display main image
-// Display address
+// Sticky header and side bar (CSS)
 
 
-
-function submitData(name, email) {
-  let formData = {
-      name: name,
-      email: email
-  };
-
-  let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(formData)
-  };
-
-  return fetch("http://localhost:3000/users", configObj)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(object) {
-          let h2 = document.createElement('h2');
-          h2.innerHTML = object.id;
-          document.body.appendChild(h2);
-          console.log(object);
-      })
-      .catch(function(error) {
-          let h3 = document.createElement('h3');
-          h3.innerHTML = error.message;
-          document.body.appendChild(h3);
-          console.log(error.message);
-      });
-} 
